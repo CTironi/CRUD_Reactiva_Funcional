15f8213f0d5346d3ef63701efea754084fe85867
CTironi <tironi741@gmail.com>
2023-01-06 17:41:04 -0300

    Iniciamos el ejercicio de: CRUD con principios reactivos y webflux

---
+++ src/main/java/com/sofka/trabajadores/CrudReactivaYFuncionalApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.trabajadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReactivaYFuncionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudReactivaYFuncionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/trabajadores/CrudReactivaYFuncionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.trabajadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReactivaYFuncionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
